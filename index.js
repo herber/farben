700c04c0e76d5312a028bef831a73ca55f18b5b4
Tobias Herber <40146720150275@litec.ac.at>
2018-02-03 19:19:23 +0100

    use ansi-format

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const ansi = require('ansi-escape-sequences');
+const ansi = require('ansi-format');
+
+const format = (str, styles) => {
+  if (!str) return '';
+
+  return styles && styles.length ? ansi(styles) + str + '\x1b[0m' : str;
+};
 
 module.exports = (text, color) => {
-  return process.stdout.isTTY ? ansi.format(text, color) : text;
+  return process.stdout.isTTY ? format(text, color) : text;
 };
